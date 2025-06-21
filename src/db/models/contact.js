7616819a34d3cea050395725c9f94b00b44d5f9c
Pokora Viacheslav <slavikpokora567@gmail.com>
2025-06-21 23:26:25 +0200

    add

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -30,5 +30,5 @@ const contactsSchema = new Schema(
   },
 );
 
-export const ContactsCollection = model('contact', contactsSchema);
+export const ContactsCollection = model('contact', contactsSchema , 'contacts');
 
