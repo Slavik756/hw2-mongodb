d8cdd89c8403d77a92a850989644945c135d3f4d
Pokora Viacheslav <slavikpokora567@gmail.com>
2025-06-22 15:40:02 +0200

    add

--- src/db/initMongoDB.js
+++ src/db/initMongoDB.js
@@ -1,7 +1,5 @@
-
 import mongoose from 'mongoose';
-
-import {getEnvVar} from '../utils/getEnvVar.js';
+import { getEnvVar } from '../utils/getEnvVar.js';
 
 export const initMongoDB = async () => {
   try {
@@ -10,10 +8,21 @@ export const initMongoDB = async () => {
     const url = getEnvVar('MONGODB_URL');
     const db = getEnvVar('MONGODB_DB');
 
+    console.log(`Connecting to MongoDB at cluster: ${url}, database: ${db}`);
+
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`
     );
+
     console.log('Mongo connection successfully established!');
+
+    mongoose.connection.on('connected', () => {
+      console.log('✅ MongoDB connected');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      console.error('❌ MongoDB connection error:', err);
+    });
   } catch (e) {
     console.log('Error while setting up mongo connection', e);
     throw e;
